c7954ae54dd9cb088cf6cbadef186fb4bc50c9f3
Marc Dwyer <envdia@gmail.com>
2019-01-02 17:33:01 -0600

    responsive design

---
+++ build/precache-manifest.11ef7f8584bb24335489b9f0980c3f93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dbb78d3c8e6e3119d2f0",
+    "url": "/static/js/main.dbb78d3c.chunk.js"
+  },
+  {
+    "revision": "0a3fb8685056cf3f93a0",
+    "url": "/static/js/1.0a3fb868.chunk.js"
+  },
+  {
+    "revision": "dbb78d3c8e6e3119d2f0",
+    "url": "/static/css/main.13d95dde.chunk.css"
+  },
+  {
+    "revision": "bd17ac2cd3ea7b1ac9defce9a1baa31a",
+    "url": "/index.html"
+  }
+];
